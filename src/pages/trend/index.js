f3f4b30b9fe14eafa4c06cff035430352f6f5483
L5250 <915016964@qq.com>
2020-03-31 16:16:25 +0800

    canvas1

--- src/pages/trend/index.js
+++ src/pages/trend/index.js
@@ -1,64 +1,218 @@
 import React, { Component } from 'react';
 import { Layout, Button, Select, Table, Divider } from 'antd';
 import { UserOutlined, CaretDownOutlined, SearchOutlined } from '@ant-design/icons'
-import './index.css'
+import './index.scss'
 
 const { Option } = Select;
 const { Header, Footer, Sider, Content } = Layout;
 class Trend extends Component {
+    // constructor() {
+    //     super();
+    //     this.canvas = React.createRef();
+    // }
+    componentDidMount() {
+        this.updateCanvas();
+    }
+
+    updateCanvas() {
+        const ctx = this.refs.canvas.getContext('2d');
+        ctx.fillRect(0, 0, 100, 100);
+    }
+
     render() {
         const posation = ["万位", "千位", "百位", "十位", "个位", "大小单双"]
         const other = ["五星走势", "五星综合", "大小", "单双", "龙虎"]
-        const columns=[
+        const columns = [
             {
-                title:"期号",
-                dataIndex:"",
-                width:"10%"
+                title: "期号",
+                dataIndex: "",
+                // width: "40px"
             },
             {
-                title:"奖号",
-                dataIndex:"",
-                width:"10%"
+                title: "奖号",
+                dataIndex: "",
+                // width: "10%"
             },
             {
-                title:"和值",
-                dataIndex:"",
-                width:"10%"
+                title: "和值",
+                dataIndex: "",
+                // width: "10%"
             },
             {
-                title:"跨度",
-                dataIndex:"",
-                width:"10%"
+                title: "跨度",
+                dataIndex: "",
+                // width: "10%"
             },
             {
-                title:"个位",
-                dataIndex:"",
-                width:"10%"
+                title: "个位",
+                dataIndex: "",
+                width: "10%",
+                padding: 0,
+                children: [
+                    {
+                        title: '0',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '1',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '2',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '3',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '4',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '5',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '6',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '7',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '8',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '9',
+                        dataIndex: 'age',
+                    },
+
+                ]
             },
             {
-                title:"个位形态分布",
-                dataIndex:"",
-                width:"10%"
+                title: "个位形态分布",
+                dataIndex: "",
+                width: "10%",
+                children: [
+                    {
+                        title: "大",
+                        dataIndex: "",
+                    },
+                    {
+                        title: "小",
+                        dataIndex: "",
+                    },
+                    {
+                        title: "奇",
+                        dataIndex: "",
+                    },
+                    {
+                        title: "偶",
+                        dataIndex: "",
+                    },
+                    {
+                        title: "质",
+                        dataIndex: "",
+                    },
+                    {
+                        title: "和",
+                        dataIndex: "",
+                    },
+                ]
             },
             {
-                title:"个位012路",
-                dataIndex:"",
-                width:"10%"
+                title: "个位012路",
+                dataIndex: "",
+                width: "10%",
+                children: [
+                    {
+                        title: "0",
+                        dataIndex: "",
+                    },
+                    {
+                        title: "1",
+                        dataIndex: "",
+                    },
+                    {
+                        title: "2",
+                        dataIndex: "",
+                    },
+                ]
             },
             {
-                title:"个位",
-                dataIndex:"",
-                width:"10%"
+                title: "个位",
+                dataIndex: "",
+                width: "10%",
+                children: [
+                    {
+                        title: "升",
+                        dataIndex: "",
+                    },
+                    {
+                        title: "平",
+                        dataIndex: "",
+                    },
+                    {
+                        title: "降",
+                        dataIndex: "",
+                    },
+                ]
             },
             {
-                title:"个位振幅",
-                dataIndex:"",
-                width:"10%"
+                title: "个位振幅",
+                dataIndex: "",
+                width: "10%",
+                children: [
+                    {
+                        title: '0',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '1',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '2',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '3',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '4',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '5',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '6',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '7',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '8',
+                        dataIndex: 'age',
+                    },
+                    {
+                        title: '9',
+                        dataIndex: 'age',
+                    },
+
+                ]
             },
         ]
         return (
             <div className="main">
-                <Layout style={{height:"100%"}}>
+                <Layout style={{ height: "100%" }}>
                     <div style={{ background: "#5757b1", height: "52px", padding: "10px 0", display: "flex", justifyContent: "space-between" }}>
                         <Button ghost type="link">
                             <UserOutlined color="#fff" />
@@ -67,7 +221,7 @@ class Trend extends Component {
                         <div>
                             <Select defaultValue="1"
                                 // suffixIcon={<CaretDownOutlined color="#fff" />}
-                                style={{ color: "#fff" }} bordered={false}>
+                                style={{ color: "#fff", fontSize: "large" }} bordered={false}>
                                 <Option value="1">腾讯十分彩</Option>
                             </Select>
                         </div>
@@ -80,8 +234,14 @@ class Trend extends Component {
                         </div>
                     </div>
                     <Content style={{ padding: "0 10px" }}>
-                        <div>
-                            <span>第{111}期</span><span>距开奖{}</span>
+                        <div style={{ height: "40px", lineHeight: "40px" }}>
+                            <span style={{ fontSize: "large" }}>第
+                            <span>{111}</span>
+                             期
+                             </span>
+                            <span>距开奖
+                                <span>{} </span>
+                            </span>
                         </div>
                         <Divider style={{ margin: "5px 0", background: "#ccc" }} />
                         <div style={{ height: "40px", lineHeight: "40px", display: "flex" }}>
@@ -92,36 +252,53 @@ class Trend extends Component {
                             <div className="prizeBall">1</div>
                             <div>开奖时间：</div>
                         </div>
-                        <div style={{ display: "flex", justifyContent: "space-between" }}>
-                            <div>定位</div>
-                            <a href="">万位</a>
-                            <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
-                            <a href="">千位</a>
-                            <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
-                            <a href="">百位</a>
-                            <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
-                            <a href="">十位</a>
-                            <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
-                            <a href="">个位</a>
-                            <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
-                            <a href="">大小单双</a>
+
+                        <div style={{ padding: "0 10px" }}>
+                            <div style={{ display: "flex", justifyContent: "space-between", height: "40px", lineHeight: "40px" }}>
+                                <div>定位</div>
+                                <a href="">万位</a>
+                                <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
+                                <a href="">千位</a>
+                                <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
+                                <a href="">百位</a>
+                                <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
+                                <a href="">十位</a>
+                                <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
+                                <a href="">个位</a>
+                                <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
+                                <a href="">大小单双</a>
+                            </div>
+                            <div style={{ display: "flex", justifyContent: "space-between", height: "40px", lineHeight: "40px" }}>
+                                <div>其他</div>
+                                <a href="">五星走势</a>
+                                <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
+                                <a href="">五星综合</a>
+                                <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
+                                <a href="">大小</a>
+                                <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
+                                <a href="">单双</a>
+                                <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
+                                <a href="">龙虎</a>
+                            </div>
                         </div>
-                        <div style={{ display: "flex", justifyContent: "space-between" }}>
-                            <div>其他</div>
-                            <a href="">五星走势</a>
-                            <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
-                            <a href="">五星综合</a>
-                            <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
-                            <a href="">大小</a>
-                            <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
-                            <a href="">单双</a>
-                            <Divider type="vertical" style={{ margin: "5px 0", background: "#ccc" }} />
-                            <a href="">龙虎</a>
+                        <div className="canvasTable">
+
+                            <Table
+                                columns={columns}
+                                className="list"
+                                style={{ width: "100%" ,position:"absolute"}}
+                                bordered
+                            // size={"small"}
+                            />
+                            <div style={{ width: "100%" ,position:"absolute"}} >
+                                <canvas ref="canvas" width="200" height="100"
+                                    style={{ border: "1px solid #000000", position: "absolute" }}>
+                                    您的浏览器不支持 HTML5 canvas 标签。
+                                    </canvas>
+                            </div>
+
                         </div>
-                        <Table 
-                            columns={columns}
-                            bordered
-                        />
+
                     </Content>
                     {/* <Footer>
 
